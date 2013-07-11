4130e63de5d5d43f8ab0b8af57f29bd35f2a2516
damienbod <damien_bod@hotmail.com>
2013-07-11 18:31:30 +0200

    some simple rules

--- MvcFluentValidation/ModelValidators/FluentDataModelValidator.cs
+++ MvcFluentValidation/ModelValidators/FluentDataModelValidator.cs
@@ -12,6 +12,10 @@ namespace MvcFluentValidation.ModelValidators
             RuleFor(x => x.ValString2)
                 .NotNull()
                 .Length(6, 100);
+
+            RuleFor(y => y.ValDouble1).InclusiveBetween(40.0, 50.0);
+
+            RuleFor(customer => customer.ApplicationSubmitted).NotEmpty().When(customer => customer.SelectedApplicationStatus > 0);
         }
         
     }
