5ce53c1662a0a06ac7ac088af54bf05602238007
damienbod <damien_bod@hotmail.com>
2013-07-11 18:05:21 +0200

    init

---
+++ MvcFluentValidation/Controllers/FluentController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using MvcFluentValidation.Models;
+
+namespace MvcFluentValidation.Controllers
+{
+    public class FluentController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(FluentDataModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                return this.RedirectToAction("Index");
+            }
+
+            return View("Index", model);
+        }
+
+    }
+}
---
+++ MvcFluentValidation/ModelValidators/FluentDataModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using MvcFluentValidation.Models;
+
+namespace MvcFluentValidation.ModelValidators
+{
+    public class FluentDataModelValidator : AbstractValidator<FluentDataModel>
+    {
+        public FluentDataModelValidator()
+        {
+           RuleFor(x => x.ValString1)
+            .NotNull();
+            RuleFor(x => x.ValString2)
+                .NotNull()
+                .Length(6, 100);
+        }
+        
+    }
+}
